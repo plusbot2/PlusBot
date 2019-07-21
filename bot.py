1ef4012947b776b5c120223faa740725f678a215
karl-police <karl-police2001@bluewin.ch>
2019-07-21 13:13:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,12 +5,6 @@ import yaml
 import os
 
 #Set globals
-r=praw.Reddit(user_agent="PlusBot by /u/captainmeta4",
-              username="plusbot2",
-              password= os.environ.get('password'),
-              client_id= os.environ.get('client_id'),
-              client_secret= os.environ.get('client_secret')
-              )
 sub = r.subreddit('karlpolicebots')
 
 
