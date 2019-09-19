71142445104a0f56a75b7126020d022dca7eedab
karl-police <karl-police2001@bluewin.ch>
2019-09-19 09:30:05 +0200

    Update bot_login.py

--- bot_login.py
+++ bot_login.py
@@ -4,7 +4,7 @@ import os
 def bot_login():
     print ("Logging in..")
     try:
-        r = praw.Reddit(username = os.environ["reddit_username"],
+        reddit = praw.Reddit(username = os.environ["reddit_username"],
                 password = os.environ["reddit_password"],
                 client_id = os.environ["client_id"],
                 client_secret = os.environ["client_secret"],
@@ -12,4 +12,4 @@ def bot_login():
         print ("Logged in!")
     except:
         print ("Failed to log in!")
-    return r
+    return reddit
