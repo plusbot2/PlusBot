4be289e24cecc728b78336e38060c8cee621deea
plusbot2 <53140997+plusbot2@users.noreply.github.com>
2019-09-21 15:56:00 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -130,6 +130,7 @@ class Bot():
             #if user has no flair, or score flair, set new score flair
             print(parent_comment.author_flair_text)
             print(parent_comment.author_flair_text.encode())
+            print(len(parent_comment.author_flair_text))
             print(parent_comment.author_flair_richtext)
             print(len(parent_comment.author_flair_richtext))
             
@@ -143,6 +144,7 @@ class Bot():
                 pass
             #checks if the array is empty or not.
             elif parent_comment.author_flair_richtext is None or parent_comment.author_flair_richtext == 0:
+                print("test2")
                 pass
             #checks if there is a css class for the flair.
             elif parent_comment.author_flair_css_class is None or len(parent_comment.author_flair_css_class) == 0:
